2e15dd8b2557f3f62bf360a9aa28ede4f08b211c
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2022-05-17 19:27:13 -0300

    Add fontawesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.scss'
 
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
