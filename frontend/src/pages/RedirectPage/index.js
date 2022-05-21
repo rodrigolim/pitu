80f1c880970643660a101689f79afd3cdcc5acf4
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2022-05-21 14:01:56 -0300

    Redirect finalizado

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import Header from '../../componentes/Header';
+import { Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ShortenerService from '../../services/shortenerServices';
 
 class RedirectPage extends React.Component{
     constructor(props){
         super(props)
+
+        this.state ={
+            isLoading: false,
+            url: '',
+            errorMessage: '',
+        }
+    }
+
+    async componentDidMount(){
+        const { code } = this.props.match.params;
+
+        try {
+            const service = new ShortenerService();
+            const { url } = await service.getLink(code);
+
+            window.location = url;
+        } catch (error) {
+            this.setState({isLoading: false, errorMessage:'A URL solicitada não existe'});            
+        }
     }
 
     render(){
         return(
-            <p>RedirectPage</p>
+            <Container>
+                <Header>Redirecionando...</Header>
+            </Container>
         )
     }
 }
